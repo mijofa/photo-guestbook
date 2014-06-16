0fea95181e1538fcc32051326ae04e192892be00
Mike Fabre <mijofa+github@fabre.id.au>
2014-06-16 21:58:32 +1000

    Improved saving of drawings. Made blank_canvas look like a notebook page. Improved the painter, & photostrip for blank_canvas

--- main.py
+++ main.py
@@ -76,7 +76,21 @@ Builder.load_string("""
 """)
 
 class blank_canvas(Widget):
-    pass
+    def __init__(self, *args):
+        super(blank_canvas, self).__init__(*args)
+        self.bind(size=self.draw_lines, pos=self.draw_lines)
+    def draw_lines(self, *args):
+        line_dist = 40
+        line_offset = 0
+        num_lines = (self.height/line_dist)
+        self.canvas.clear()
+        with self.canvas:
+            Color(0,0,0,1)
+            for i in xrange(1,int(num_lines)):
+                line_offset += line_dist
+                Line(width=1, points=[self.pos[0], self.pos[1]+line_offset, self.pos[0]+self.width, self.pos[1]+line_offset])
+            Color(1,0,0,0.25)
+            Line(width=1.5, points=[self.pos[0]+(line_dist*1.5), self.pos[1]+self.height, self.pos[0]+(line_dist*1.5), self.pos[1]])
 
 class FileChooserGalleryView(FileChooserIconView):
     thumbsize = dp(119)
@@ -107,7 +121,6 @@ class FileChooserGalleryView(FileChooserIconView):
     def get_time(self, ctx):
         return time.ctime(os.path.getmtime(ctx.path))
 
-
 class PaintWidget(Widget):
     def save_png(self, filename):
         ### Kivy 1.8.1 has an export_to_png function in the widget class. I'm not using 1.8.1 so I'm writing my own.
@@ -126,7 +139,7 @@ class PaintWidget(Widget):
         try:
             fbo.texture.save(filename)
             success = True
-            kivy.logger.Logger.debug("PaintWidget: Saved file %s" % filename)
+            kivy.logger.Logger.info("PaintWidget: Saved file %s" % filename)
         except Exception as e:
             success = False
             kivy.logger.Logger.error("PaintWidget: Can't save file: %s" % filename)
@@ -142,45 +155,97 @@ class PaintWidget(Widget):
         with self.canvas:
             Color(1,1,1)
             diameter = 4
-            Ellipse(pos=(touch.x - diameter / 2, touch.y - diameter / 2), size=(diameter, diameter))
+            Ellipse(pos=(touch.x-(diameter/2), touch.y-(diameter/2)), size=(diameter, diameter))
             touch.ud['line_outer'] = Line(points=(touch.x, touch.y), width=diameter)
             Color(0,0,0)
+            Ellipse(pos=(touch.x-((diameter-1)/2), touch.y-((diameter-1)/2)), size=(diameter-1, diameter-1))
             touch.ud['line_inner'] = Line(points=touch.ud['line_outer'].points, width=diameter-1)
     def on_touch_move(self, touch):
         if touch.ud.has_key('line_outer'):
             touch.ud['line_outer'].points += [touch.x, touch.y]
         if touch.ud.has_key('line_inner'):
             touch.ud['line_inner'].points = touch.ud['line_outer'].points
+    def clear(self):
+        self.canvas.clear()
+
+class PaintScreen(Screen):
+    def update_rect(self, instance, value):
+        instance.bg_col.size = instance.size
+        instance.bg_col.pos = instance.pos
+    def __init__(self, *args, **kwargs):
+        super(PaintScreen, self).__init__(*args, **kwargs)
+        self.source = ''
+        with self.canvas:
+            Color(0,0,0,1)
+            self.bg_col = Rectangle()
+
+        self.blank = blank_canvas()
+        self.blank.source = self.source
+        self.add_widget(self.blank)
+        self.blank.opacity = 1
+
+        self.img = Image(source=self.source, allow_stretch=True)
+        self.add_widget(self.img)
+
+        self.painter = PaintWidget()
+        self.add_widget(self.painter)
+
+        self.bind(size=self.update_rect, pos=self.update_rect)
+    def set_image(self, bg_filename, *args):
+        self.source = bg_filename
+        self.img.source = self.source
+
+        if os.path.exists(self.source):
+            self.blank.opacity = 0
+            self.img.opacity = 1
+        else:
+            self.blank.opacity = 1
+            self.img.opacity = 0
 
 class image_button(Button):
     def __init__(self, source = '', *args, **kwargs):
+        self.source = source
         super(image_button, self).__init__(*args, **kwargs)
-        if type(source) == str:
-            self.img = Image(source=source, allow_stretch=True)
-        else:
-            self.img = source
+
+        self.blank = blank_canvas()
+        self.blank.source = self.source
+        self.add_widget(self.blank)
+
+        self.img = Image(source=self.source, allow_stretch=True)
         self.add_widget(self.img)
+
+        if os.path.exists(self.source):
+            self.blank.opacity = 0
+            self.img.opacity = 1
+        else:
+            self.blank.opacity = 1
+            self.img.opacity = 0
+
         self.bind(size=self.update_img, pos=self.update_img)
     def update_img(self, instance, value):
+        instance.blank.size = instance.size
+        instance.blank.pos = instance.pos
         instance.img.size = instance.size
         instance.img.pos = instance.pos
     def set_image(self, filename):
-        self.img.source = filename
+        self.source = filename
+        self.img.source = self.source
+        if os.path.exists(self.source):
+            self.blank.opacity = 0
+            self.img.opacity = 1
+        else:
+            self.blank.opacity = 1
+            self.img.opacity = 0
 
 class PhotoStrip(ScrollView):
-    def set_path(self, path):
-        self.path = path
-        self.image0.set_image(os.path.join(path, '0.jpg'))
-        self.image1.set_image(os.path.join(path, '1.jpg'))
-        self.image2.set_image(os.path.join(path, '2.jpg'))
-    def clear_path(self, *args):
-        self.path = None
-        source = ''
-        self.image0.set_image(source)
-        self.image1.set_image(source)
-        self.image2.set_image(source)
+    def on_press(self, filename):
+        pass
+    def on_release(self, filename):
+        pass
     def __init__(self, *args, **kwargs):
         super(PhotoStrip, self).__init__(size_hint=(1,1), pos_hint={'center_x': 0.5, 'center_y': 0.5}, do_scroll_x=False, *args, **kwargs)
+        self.register_event_type('on_press')
+        self.register_event_type('on_release')
         self.strip = Image(source='photos-strip.png', allow_stretch=True, size_hint_y=1.8)
         self.add_widget(self.strip)
 
@@ -193,15 +258,29 @@ class PhotoStrip(ScrollView):
         self.image2 = image_button()
         self.image2.bind(on_press=self.press_btn, on_release=self.release_btn)
         self.strip.add_widget(self.image2)
-        self.image3 = image_button(source=blank_canvas())
+        self.image3 = image_button()
         self.image3.bind(on_press=self.press_btn, on_release=self.release_btn)
         self.strip.add_widget(self.image3)
 
         self.strip.bind(size=self.update_buttons,pos=self.update_buttons)
-    def press_btn(self, *args):
-        kivy.logger.Logger.debug('PhotoStrip: pressed an image button: '+str(args))
-    def release_btn(self, *args):
-        kivy.logger.Logger.debug('PhotoStrip: released an image button: '+str(args))
+    def set_path(self, path):
+        self.path = path
+        self.image0.set_image(os.path.join(path, '0.jpg'))
+        self.image1.set_image(os.path.join(path, '1.jpg'))
+        self.image2.set_image(os.path.join(path, '2.jpg'))
+        self.image3.set_image(os.path.join(path, 'blank'))
+    def clear_path(self, *args):
+        self.scroll_y = 1
+        self.effect_y.value = self.effect_y.min # This is to work around a bug with the ScrollView (https://github.com/kivy/kivy/issues/2038)
+        self.path = None
+        source = ''
+        self.image0.set_image(source)
+        self.image1.set_image(source)
+        self.image2.set_image(source)
+    def press_btn(self, btn):
+        self.dispatch('on_press', btn.source)
+    def release_btn(self, btn):
+        self.dispatch('on_release', btn.source)
     def update_buttons(self, instance, value):
         spacing = 20
         padding = 25
@@ -213,36 +292,53 @@ class PhotoStrip(ScrollView):
             offset += img.height+spacing
 
 class Main(App):
+    def pressed_win(self, *args):
+        if self.screen_manager.current == 'painter':
+            savedir = os.path.normpath(self.paint_screen.painter.source+'.overlays'+os.path.sep)
+            if not os.path.isdir(savedir):
+                os.mkdir(savedir)
+            index = 0
+            filename = '%02d.png'
+            dircontents = os.listdir(savedir)
+            while filename % index in dircontents:
+                index += 1
+            self.paint_screen.painter.save_png(os.path.join(savedir, filename % index))
+        return True
+    def pressed_home(self, *args):
+        if self.screen_manager.current == 'painter':
+            self.goto_screen('photostrip', 'right')
+        elif self.screen_manager.current == 'chooser':
+            self.chooser._show_progress()
+            self.chooser._trigger_update()
+        return True
     def pressed_back(self, *args):
         if self.screen_manager.current == 'photostrip':
-            self.screen_manager.transition.direction = 'right'
-            self.screen_manager.current = 'chooser'
+            self.goto_screen('chooser', 'right')
         elif self.screen_manager.current == 'chooser':
             if os.path.samefile(self.chooser.path, PHOTOS_PATH):
                 return
             self.chooser.rootpath = os.path.normpath(os.path.join(self.chooser.path, os.path.pardir))
             self.chooser.path = self.chooser.rootpath
         return True
-    def pressed_home(self, *args):
-        if self.screen_manager.current == 'photostrip':
-            pass
-#            self.painter.save_png('/sdcard/DCIM/tmp/blah.png')
-        elif self.screen_manager.current == 'chooser':
-            self.chooser._show_progress()
-            self.chooser._trigger_update()
-        return True
-    def pressed_win(self, *args):
-        pass
+    def goto_screen(self, screen_name, direction):
+        self.screen_manager.transition.direction = direction
+        self.screen_manager.current = screen_name
     def select_folder(self, chooser):
-        self.screen_manager.transition.direction = 'left'
-        self.screen_manager.current = 'photostrip'
+        self.goto_screen('photostrip', 'left')
         self.photostrip.set_path(chooser.current_entry.path)
     def enter_chooser(self, *args):
+        self.win_btn.text = ''
         self.home_btn.text = 'Refresh'
         self.back_btn.text = '<-'
+        self.photostrip.clear_path()
     def enter_strip(self, *args):
+        self.win_btn.text = ''
         self.home_btn.text = ''
         self.back_btn.text = '<-'
+    def enter_painter(self, *args):
+        self.win_btn.text = 'Save'
+        self.home_btn.text = 'X'
+        self.back_btn.text = ''
     def build(self):
         root = FloatLayout()
         self.screen_manager = ScreenManager(transition=SlideTransition(), size_hint=[0.925,1],pos_hint={'left': 1})
@@ -253,14 +349,15 @@ class Main(App):
         # I'd like to have 'back', 'refresh', 'save', & 'cancel' buttons. I don't know how well I can make this work on the navbar though.
         self.win_btn = Button(text='', size_hint=[0.075,0.1],pos_hint={'right': 1, 'center_y': 0.8})
         self.win_btn.bind(on_press=self.pressed_win)
-        root.add_widget(self.win_btn)
 
         self.home_btn = Button(text='', size_hint=[0.075,0.1],pos_hint={'right': 1, 'center_y': 0.5})
         self.home_btn.bind(on_press=self.pressed_home)
-        root.add_widget(self.home_btn)
 
         self.back_btn = Button(text='', size_hint=[0.075,0.1],pos_hint={'right': 1, 'center_y': 0.2})
         self.back_btn.bind(on_press=self.pressed_back)
+
+        root.add_widget(self.win_btn)
+        root.add_widget(self.home_btn)
         root.add_widget(self.back_btn)
 
         ## FileChooser
@@ -269,7 +366,6 @@ class Main(App):
         self.chooser = FileChooserGalleryView(rootpath=PHOTOS_PATH)
         self.chooser.bind(on_select_folder=self.select_folder)
         chooser_screen.add_widget(self.chooser)
-        self.screen_manager.add_widget(chooser_screen)
         def update_rect(instance, value):
             instance.bg.pos = instance.pos
             instance.bg.size = instance.size
@@ -281,12 +377,19 @@ class Main(App):
         photostrip_screen = Screen(name='photostrip')
         photostrip_screen.bind(on_enter=self.enter_strip)
         self.photostrip = PhotoStrip()
-        photostrip_screen.bind(on_leave=self.photostrip.clear_path)
         photostrip_screen.add_widget(self.photostrip)
-#        self.painter = PaintWidget()
-#        viewer_screen.bind(on_leave=lambda args: self.painter.canvas.clear())
-#        viewer_screen.add_widget(self.painter)
+
+        self.paint_screen = PaintScreen(name='painter')
+        self.paint_screen.bind(on_enter=self.enter_painter)
+        self.paint_screen.bind(on_leave=lambda src: self.paint_screen.painter.clear())
+        self.photostrip.bind(
+                on_press=lambda src, fn: self.paint_screen.set_image(fn),
+                on_release=lambda src, fn: self.goto_screen('painter', 'left'),
+        )
+
+        self.screen_manager.add_widget(chooser_screen)
         self.screen_manager.add_widget(photostrip_screen)
+        self.screen_manager.add_widget(self.paint_screen)
 
         return root
 
